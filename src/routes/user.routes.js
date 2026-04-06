79ddce30fec30f4db164b62853c438fbc130b5bf
samim1920 <sksamimali2021@gmail.com>
2026-04-06 18:21:39 +0530

    Add user profile management routes for password change, current user retrieval, and updates

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
-import { refreshAccessToken, userLogin, userLogout, userRegister } from "../controllers/user.controller.js";
+import { refreshAccessToken, userLogin, userLogout, userRegister,
+    ChangecurrentPassword, getcurrentuser, updateuser, 
+    updateuseravatar, updateusercoverimage, 
+    getwatchHistory} from "../controllers/user.controller.js";
 import { veryfyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 
@@ -26,6 +29,17 @@ router.route("/login").post(userLogin)
 
 router.route("/logout").post(veryfyJWT,userLogout)
 router.route("/refreshtoken").post(refreshAccessToken)
+router.route("/changecurrentpassword").post(veryfyJWT,ChangecurrentPassword)
+router.route("/currentuser").get(veryfyJWT,getcurrentuser)
+router.route("/udateuser").patch(veryfyJWT,updateuser)  // .patgh ksron akta user update hho66e tai
+
+router.route("/updateavatar").patch(veryfyJWT,upload.single("avatar"),updateuseravatar)  // 2 ta middleware kron akta file upload hobe
+
+router.route("/updatecoverimage").patch(veryfyJWT,upload.single("coverImage",updateusercoverimage))
+router.route("/c/:username").get(veryfyJWT,getchalleldetails)
+router.route("/history").get(veryfyJWT,getwatchHistory)
+
+
 
 
 export default router
